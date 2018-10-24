ef9217eedbc983c351c570b4b9d4fff381d6a8d9
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-24 08:58:40 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ]
 
+const gran = React.createElement('p', {}, "Two grannies having the time of their life!")
+const p = React.createElement('p', {}, "Passegers:")
+const agnes = React.createElement('li', {}, "Agnes") 
+const muriel = React.createElement('li', {}, "Muriel")
+const list = React.createElement('ul', {}, [agnes, muriel])
+
 // Define these exported classes
 export class OlderCoaster extends Component {
   render() {
