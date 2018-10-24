4758d343f56e0bba0eea7764691619144b59a0f8
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-24 09:12:07 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -46,7 +46,9 @@ export class App extends Component {
   render() {
     return (
       <div id="app">
-
+        {OlderCoaster}
+        {InFrontOfYou}
+        {ButcherShop}
       </div>
     )
   }
