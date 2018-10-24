fe3e18766288dcd4bc51b3119241996bb62dc62c
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-24 08:58:47 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const BUTCHER_PRODUCTS = [
 
 const gran = React.createElement('p', {}, "Two grannies having the time of their life!")
 const p = React.createElement('p', {}, "Passegers:")
-const agnes = React.createElement('li', {}, "Agnes") 
+const agnes = React.createElement('li', {}, "Agnes")
 const muriel = React.createElement('li', {}, "Muriel")
 const list = React.createElement('ul', {}, [agnes, muriel])
 
