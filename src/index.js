6508d090f4198b61793fac88af605a5466adbdcf
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-24 09:10:56 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -33,8 +33,8 @@ export class InFrontOfYou extends React.Component {
 export class ButcherShop extends React.Component {
   render() {
     return React.createElement('div', {className: "butcher-shop"}, [
-      React.createElement('p', {}, "Hello! We have the following products for sale today:"), 
-      React.createElement('ul', {}, BUTCHER_PRODUCTS.map(name => 
+      React.createElement('p', {}, "Hello! We have the following products for sale today:"),
+      React.createElement('ul', {}, BUTCHER_PRODUCTS.map(name =>
       React.createElement('li', {}, name)
     ))
     ])
